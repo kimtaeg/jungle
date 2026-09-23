25957fc4e280d5edf360a281a56c1d1e9dbe630e
kimtaeg <tank_1017@naver.com>
2026-09-24 00:20:03 +0900

    5주차 7번 풀이

--- week5/debugging_lab_docker/challenges/07_stack_use_after_return/bug.c
+++ week5/debugging_lab_docker/challenges/07_stack_use_after_return/bug.c
@@ -53,15 +53,16 @@ static void view_set(LineView *out, char **arr, int n) {
 }
 
 static void split_lines(LineView *out, char *text) {
-    char *parts[MAX_LINES];              
+    char **arr = malloc(sizeof(char *) * MAX_LINES);            
     int n = 0;
     /* strtok는 새로 할당하지 않고, 넘겨받은 문자열 내부의 주소를 돌려준다. 
     * 따라서, strtok은 원본 버퍼를 제자리에서 수정한다. 
     */
     for (char *ln = strtok(text, "\n"); ln && n < MAX_LINES; ln = strtok(NULL, "\n"))
-        parts[n++] = ln;
+        arr[n++] = ln;
 
-    view_set(out, parts, n);      
+    out->lines = arr;
+    out->count = n;     
 
     /* TODO 상기 코드를 수정하여 결과를 호출자가 준 out 에 직접 채운다(값 반환 아님, 지역 주소 반환 아님). */       
 }
@@ -87,5 +88,7 @@ int main(void) {
         checksum += (unsigned char)v.lines[i][0];
 
     printf("lines = %d, checksum = %ld\n", v.count, checksum);
+
+    free(v.lines);
     return 0;
 }
